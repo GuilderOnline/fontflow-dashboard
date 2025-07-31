da903a075bd5f7a55d909da0bc9d637091c8361c
GuilderOnline <niall@guilderdesign.com>
2025-07-31 15:08:59 +0100

    api

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,13 +1,12 @@
 // src/utils/api.js
-
 let API_BASE = '';
 
 if (window.location.hostname === 'localhost') {
   // ✅ Local backend
   API_BASE = 'http://localhost:4000/api';
 } else {
-  // ✅ Production backend
-  API_BASE = 'https://fontflow-production-2412.up.railway.app/api';
+  // ✅ Production backend (Render)
+  API_BASE = 'https://fontflow-backend.onrender.com/api';
 }
 
 export default API_BASE;
