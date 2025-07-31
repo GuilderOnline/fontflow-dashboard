238b81bdcdf531bb65de5adf521cf971b3c346c9
GuilderOnline <niall@guilderdesign.com>
2025-07-31 17:18:20 +0100

    dashboard button edit

--- src/pages/DashboardPage.js
+++ src/pages/DashboardPage.js
@@ -164,12 +164,34 @@ const DashboardPage = () => {
                   <td>{new Date(font.createdAt).toLocaleString()}</td>
                   <td>
   <button onClick={() => openPreview(font)}>Preview</button>{' '}
-  <a href={font.originalDownloadUrl} download>
-    <button>Download Original</button>
-  </a>{' '}
-  <a href={font.woff2DownloadUrl} download>
-    <button>Download WOFF2</button>
-  </a>{' '}
+  <a
+  href={font.originalDownloadUrl}
+  download
+  style={{
+    padding: '5px 10px',
+    background: '#4cafef',
+    color: 'white',
+    borderRadius: '4px',
+    textDecoration: 'none',
+    display: 'inline-block',
+  }}
+>
+  Download Original
+<a
+  href={font.woff2DownloadUrl}
+  download
+  style={{
+    padding: '5px 10px',
+    background: '#28a745',
+    color: 'white',
+    borderRadius: '4px',
+    textDecoration: 'none',
+    display: 'inline-block',
+  }}
+>
+  Download WOFF2
+</a>
+{' '}
   <button onClick={() => deleteFont(font._id)}>Delete</button>
 </td>
                 </tr>
