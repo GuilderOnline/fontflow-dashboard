ef9a38975ef37eeef26ae2cff99abc9b4d1ca974
GuilderOnline <niall@guilderdesign.com>
2025-08-03 12:14:59 +0100

    css

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -15,10 +15,10 @@ const Sidebar = () => {
     <aside className="sidebar">
       <div className="sidebar-logo"><img src="/fontflow-logo.png" alt="FontFlow Logo"/></div>
       <ul className="sidebar-menu">
-        <li onClick={() => navigate('/dashboard')}>Dashboard</li>
-        <li onClick={() => navigate('/upload')}>Upload Font</li>
-        <li onClick={() => navigate('/projects')}>Projects</li>
-        <li onClick={handleLogout}>Logout</li>
+        <li id ="d-link" onClick={() => navigate('/dashboard')}>Dashboard</li>
+        <li id ="u-link" onClick={() => navigate('/upload')}>Upload Font</li>
+        <li id ="p-link" onClick={() => navigate('/projects')}>Projects</li>
+        <li id ="l-link" onClick={handleLogout}>Logout</li>
       </ul>
     </aside>
   );
